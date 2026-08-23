50b478bab817ed0b4bcb263390cd1681cf824be7
kerouanton <18640109+kerouanton@users.noreply.github.com>
2026-08-23 17:59:30 +0200

    Add gpt_exporter package

---
+++ gpt_exporter/__init__.py
@@ -0,0 +1,9 @@
+"""Reusable library API for GPT Exporter."""
+
+from .paths import ArchivePaths, default_archive_paths, default_user_profile
+
+__all__ = [
+    "ArchivePaths",
+    "default_archive_paths",
+    "default_user_profile",
+]
