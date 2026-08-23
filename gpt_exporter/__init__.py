0470779076432cca471f462f68a7d4f6ab76a170
kerouanton <18640109+kerouanton@users.noreply.github.com>
2026-08-23 21:25:11 +0200

    Expose central package version

--- gpt_exporter/__init__.py
+++ gpt_exporter/__init__.py
@@ -1,9 +1,11 @@
 """Reusable library API for GPT Exporter."""
 
 from .paths import ArchivePaths, default_archive_paths, default_user_profile
+from .version import __version__
 
 __all__ = [
     "ArchivePaths",
+    "__version__",
     "default_archive_paths",
     "default_user_profile",
 ]
