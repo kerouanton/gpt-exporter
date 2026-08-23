a83adb882aa6384196002ff75049cffb9da524fe
kerouanton <18640109+kerouanton@users.noreply.github.com>
2026-08-23 19:59:40 +0200

    Export browser bundle import API

--- gpt_exporter/archive/__init__.py
+++ gpt_exporter/archive/__init__.py
@@ -1,5 +1,6 @@
 """Archive analysis and storage helpers for GPT Exporter."""
 
+from .importer import ImportBundleResult, import_bundle
 from .inventory import (
     InventoryResult,
     collect_media_inventory,
@@ -10,8 +11,10 @@ from .inventory import (
 )
 
 __all__ = [
+    "ImportBundleResult",
     "InventoryResult",
     "collect_media_inventory",
+    "import_bundle",
     "inventory_media",
     "render_console_summary",
     "render_text_report",
