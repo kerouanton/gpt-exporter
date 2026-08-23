d586ab779033a38052f5be9960ba0dc61a55a930
kerouanton <18640109+kerouanton@users.noreply.github.com>
2026-08-23 19:31:32 +0200

    Export batch orchestration API

--- gpt_exporter/export/__init__.py
+++ gpt_exporter/export/__init__.py
@@ -1,11 +1,14 @@
 """Reusable export APIs for GPT Exporter."""
 
+from .batch import BatchExportResult, export_batch
 from .docx import DocxExportResult, export_docx
 from .markdown import MarkdownExportResult, export_markdown
 
 __all__ = [
+    "BatchExportResult",
     "DocxExportResult",
     "MarkdownExportResult",
+    "export_batch",
     "export_docx",
     "export_markdown",
 ]
