8dad17f3cd2b7fca890e91d4737687e44cc1442b
kerouanton <18640109+kerouanton@users.noreply.github.com>
2026-08-31 17:56:52 +0200

    Core: expose normalized batch export API

--- gpt_exporter/export/__init__.py
+++ gpt_exporter/export/__init__.py
@@ -4,6 +4,7 @@ from .batch import BatchExportResult, export_batch
 from .docx import DocxExportResult, export_docx
 from .markdown import MarkdownExportResult, export_markdown
 from .normalized import NormalizedExportResult, export_normalized_conversation
+from .normalized_batch import export_normalized_batch
 from .normalized_markdown import render_conversation_markdown
 
 __all__ = [
@@ -14,6 +15,7 @@ __all__ = [
     "export_batch",
     "export_docx",
     "export_markdown",
+    "export_normalized_batch",
     "export_normalized_conversation",
     "render_conversation_markdown",
 ]
