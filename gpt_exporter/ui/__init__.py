cae13cbf8163fae8e8cdfc30a33e3779d75cb447
kerouanton <18640109+kerouanton@users.noreply.github.com>
2026-08-31 12:04:38 +0200

    UI: export workspace archive dialog

--- gpt_exporter/ui/__init__.py
+++ gpt_exporter/ui/__init__.py
@@ -3,11 +3,13 @@
 from .help_dialogs import show_about_dialog
 from .markdown_viewer import MarkdownSegment, MarkdownViewer, markdown_segments, show_markdown_document
 from .provider_manager import ProviderManagerDialog, show_provider_manager
+from .workspace_archive_dialog import WorkspaceArchiveDialog
 
 __all__ = [
     "MarkdownSegment",
     "MarkdownViewer",
     "ProviderManagerDialog",
+    "WorkspaceArchiveDialog",
     "markdown_segments",
     "show_about_dialog",
     "show_markdown_document",
